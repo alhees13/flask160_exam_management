7b8fbd9cc5b3fa8c265fd798e23888572cdac9d9
Jsgalvan <168999675+Jsgalvan@users.noreply.github.com>
2024-05-17 03:19:20 -0400

    sdvsv

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask_session import Session
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from cs50 import SQL
@@ -14,6 +13,23 @@ Session(app)
 
 db = SQL ( "sqlite:///school.db" )
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, id, username, password, role):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.role = role
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = db.execute("SELECT * FROM User WHERE id = ?", user_id)
+    if user:
+        return User(id=user[0]['id'], username=user[0]['username'], password=user[0]['password'], role=user[0]['role'])
+    return None
+
 
 # Routes
 @app.route('/')
@@ -25,11 +41,9 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        role = 'student'  # Default role is student, can be extended for teacher role
-        hashed_password = generate_password_hash(password, method='sha256')
-        new_user = User(username=username, password=hashed_password, role=role)
-        db.session.add(new_user)
-        db.session.commit()
+        role = request.form['role']  # Get role from form
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        db.execute("INSERT INTO User (username, password, role) VALUES (?, ?, ?)", username, hashed_password, role)
         flash('Registration successful!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html')
@@ -39,9 +53,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and check_password_hash(user.password, password):
-            login_user(user)
+        user = db.execute("SELECT * FROM User WHERE username = ?", username)
+        if user and check_password_hash(user[0]['password'], password):
+            login_user(User(id=user[0]['id'], username=user[0]['username'], password=user[0]['password'], role=user[0]['role']))
             return redirect(url_for('home'))
         flash('Login unsuccessful. Please check your username and password.', 'danger')
     return render_template('login.html')
